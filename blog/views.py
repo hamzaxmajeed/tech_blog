0f490db68ab987cfe0e0c0c5e340ecf5d9c32121
Hamza <hamza.majeed00@hotmail.co.uk>
2021-02-06 13:03:30 +0000

    added db models and posts

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Hamza Majeed',
-        'title': 'Tech Blog 1',
-        'content': 'First post content',
-        'date_posted': 'February 01, 2021'
-    },
-    {
-        'author': 'Hamza Majeed',
-        'title': 'Tech Blog 2',
-        'content': 'Second post content',
-        'date_posted': 'February 02, 2021'
-    }
-]
-
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
