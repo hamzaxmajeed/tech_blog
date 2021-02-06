3ffb93ea5e26a7086b8c9b338c4538d86cc5eb6d
Hamza <hamza.majeed00@hotmail.co.uk>
2021-02-06 11:17:45 +0000

    added base template, css and bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+posts = [
+    {
+        'author': 'Hamza Majeed',
+        'title': 'Tech Blog 1',
+        'content': 'First post content',
+        'date_posted': 'February 01, 2021'
+    },
+    {
+        'author': 'Hamza Majeed',
+        'title': 'Tech Blog 2',
+        'content': 'Second post content',
+        'date_posted': 'February 02, 2021'
+    }
+]
 
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html', {'title': 'About'})
